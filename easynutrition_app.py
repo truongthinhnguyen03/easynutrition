d5462a7e80e165f0e553ca28ee02e807b6664e7a
truongthinhnguyen03 <121245100+truongthinhnguyen03@users.noreply.github.com>
2023-10-16 22:11:56 +0000

    Update easynutrition_app & requirements

--- easynutrition_app.py
+++ easynutrition_app.py
@@ -1,16 +1,23 @@
 import streamlit as st
 from langchain import PromptTemplate, LLMChain, HuggingFaceHub
+import os
 
-st.title('🦜🔗 Easy Nutrition')
+st.title('🍽️🥗🧘🏻‍♀️ Easy Nutrition - Proof of Concept')
 
-HUGGINGFACEHUB_API_TOKEN = st.sidebar.text_input('HuggingFace API Token')
+st.markdown('''Easy Nutrition: Decode Your Food Labels! Are you ready to take control of your health? Take the first step to understanding what\'s on your plate.''')
+st.divider()
+
+with st.sidebar.form(key ='TokenForm'):
+    HUGGINGFACEHUB_API_TOKEN = st.text_input('HuggingFace API Token', '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', type='password', disabled=True)    
+    submitted_Token = st.form_submit_button(label = 'Submit')
+
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = HUGGINGFACEHUB_API_TOKEN
 
 def generate_response(input_text):
   template = """
   You are a nutritionist;
   You are given an ingredient list;
-  You can generate an EXPLANATION using simple term to explain the possible origin of each ingredient
-  and what type of food this ingredient list comes from.
+  You can generate EXPLANATION using simple terms to explain the possible origin of each ingredient - New line for each ingredient.
 
   LIST: {input_text}
   EXPLAINATION:
@@ -21,17 +28,23 @@ def generate_response(input_text):
   repo_id = "HuggingFaceH4/zephyr-7b-alpha"
 
   llm = HuggingFaceHub(
-    repo_id=repo_id, model_kwargs={"max_new_tokens":256, "do_sample":True, "temperature":0.7, "top_k":50, "top_p":0.95}
+    repo_id=repo_id, model_kwargs={"max_new_tokens":512, "do_sample":True, "temperature":0.7, "top_k":50, "top_p":0.95}
   )
 
   llm_chain = LLMChain(prompt=prompt, llm=llm)
 
   st.info(llm_chain.run(input_text))
 
+examples = {
+    'Example 1': 'Sugar, Invert sugar, corn syrup, modified corn starch, citric acid, tartaric acid, natural and artificial flavour, titanium dioxide, red 40, yellow 5, and blue 1',
+    'Example 2': 'Enriched unbleached flour (wheat flour, malted barley flour, ascorbic acid [dough conditioner], niacin, reduced iron, thiamin mononitrate, ribflavin, folic acid), sugar, degermed yellow cornmeal, salt, leavening (baking soda, sodium acid pyrophosphate), soybean oil, honey powder, natural flavor'
+  }
+select_example = st.selectbox("Select an example", list(examples.keys()))
+
 with st.form('my_form'):
-  text = st.text_area('Enter text:', 'Sugar, Invert sugar, corn syrup, modified corn starch, citric acid, tartaric acid, natural and artificial flavour, titanium dioxide, red 40, yellow 5, and blue 1')
+  text = st.text_area('Enter nutrition list:', examples[select_example], height=150)
   submitted = st.form_submit_button('Submit')
-  if not HUGGINGFACEHUB_API_TOKEN.startswith('hf-'):
+  if not HUGGINGFACEHUB_API_TOKEN.startswith('hf_'):
     st.warning('Please enter your HuggingFace API key!', icon='⚠')
-  if submitted and HUGGINGFACEHUB_API_TOKEN.startswith('hf-'):
+  if submitted and HUGGINGFACEHUB_API_TOKEN.startswith('hf_'):
     generate_response(text)
