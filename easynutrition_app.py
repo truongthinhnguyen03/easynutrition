6df8ddd4a893898ba2583e380e2981b147456d7d
truongthinhnguyen03 <121245100+truongthinhnguyen03@users.noreply.github.com>
2023-10-17 07:50:42 +0000

    Add spinner

--- easynutrition_app.py
+++ easynutrition_app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 from langchain import PromptTemplate, LLMChain, HuggingFaceHub
 import os
+import time
 st.set_page_config(initial_sidebar_state='collapsed')
 
 st.title('🍽️🥗🧘🏻‍♀️ Easy Nutrition - Proof of Concept')
 
-st.markdown('''Easy Nutrition: Decode Your Food Labels! Are you ready to take control of your health? Take the first step to understanding what\'s on your plate.''')
+st.code(
+'''Easy Nutrition: Decode Your Food Labels!
+Are you ready to take control of your health?
+Take the first step to understanding what\'s on your plate.''')
 st.divider()
 
 with st.sidebar.form(key ='TokenForm'):
@@ -17,7 +21,7 @@ os.environ["HUGGINGFACEHUB_API_TOKEN"] = HUGGINGFACEHUB_API_TOKEN
 def generate_response(input_text):
   template = """
   You are a nutritionist;
-  You are given an ingredient list;
+  You are given a nutrition list;
   You can generate EXPLANATION using simple terms to explain the possible origin of each ingredient - New line for each ingredient.
 
   LIST: {input_text}
@@ -33,7 +37,6 @@ def generate_response(input_text):
   )
 
   llm_chain = LLMChain(prompt=prompt, llm=llm)
-
   st.info(llm_chain.run(input_text))
 
 col1, col2 = st.columns([3, 1])
@@ -52,7 +55,9 @@ with col1:
     else:
       text = st.text_area('Enter nutrition list:', examples[select_example], height=150, placeholder="Enter ingredienct list or select from examples")
     submitted = st.form_submit_button('Submit')
-    if not HUGGINGFACEHUB_API_TOKEN.startswith('hf_'):
-      st.warning('Please enter your HuggingFace API key!', icon='⚠')
-    if submitted and HUGGINGFACEHUB_API_TOKEN.startswith('hf_'):
-      generate_response(text)
+    
+
+with st.spinner('Wait for it...'):
+  time.sleep(5)
+if submitted:
+  generate_response(text)
